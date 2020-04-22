0eb53db56a20d502bfc5959b7ccea3f9c4938bb4
Fmont88 <64174359+Fmont88@users.noreply.github.com>
2020-04-22 20:44:53 -0300

    Add files via upload

---
+++ Lista 3 Ex1.c
@@ -0,0 +1,28 @@
+//EXERCÍCIO 1
+/*1. Escreva um algoritmo que leia um inteiro positivo e imprima o resultado das potencias de r, de 0 até 10 (2°, z'.... r)
+*/
+#include <stdio.h>
+
+int main() {
+  int x,        // base
+      potencia, // guarda as potencias
+      contador;
+
+  printf("\nDigite um numero inteiro para ver até sua potencia de 10: ");
+  scanf("%d", &x);
+
+  /* Inicializacoes */
+  potencia = 1;
+  contador = 0;
+  printf("\nO valor de %d elevado a %d: %d\n", x, contador, potencia);
+  contador = 1;
+
+  /* Calculo da potencia */
+  while (contador <= 10) {
+
+        potencia = potencia * x;
+        printf("\nO valor de %d elevado a %d: %d\n", x, contador, potencia);
+        contador = contador + 1;
+  }
+  return 0;
+}
